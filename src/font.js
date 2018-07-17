99ac356fb33cfefafff5bf6487be19d9acefa7c9
Joe Bentley <jbentley@star.sr.bham.ac.uk>
2018-07-17 21:46:06 +0100

    Initial commit

---
+++ src/font.js
@@ -0,0 +1,45 @@
+const CHAR_HEIGHT = 12; // 20; // 12;
+const CHAR_WIDTH = 10; // 20; // 8;
+const NUM_PER_ROW = 16;
+
+const CHARS = 'space dwarf dwarf2 heart diamond club spade circle emptycircle ring emptyring male female note1 note2 gem '
++ 'sloperight slopeleft updown alert pagemark sectionmark thickbottom updown2 up down right left boxbottomleft leftright slopeup slopedown '
++ 'space2 ! " # $ % & \' ( ) * + , - . / '
++ '0 1 2 3 4 5 6 7 8 9 : ; < = > ? '
++ '@ A B C D E F G H I J K L M N O P Q R S T U V W X Y Z [ \\ ] ^ _ '
++ '` a b c d e f g h i j k l m n o p q r s t u v w x y z { : } ~ triangle '
++ 'accentC accentu accente accenta accenta2 accenta3 accenta4 accentc accente2 accente3 accente4 accenti accenti2 accenti3 accentA  accentA2 '
++ 'accentE accentae accentAE accento accento2 accento3 accentu2 accentu3 accenty accentO accentU cent pound yen Pt function '
++ 'accenta5 accenti4 accento4 accentu4 accentn accentN aoverbar ooverbar qmark2 boxtopleft boxtopright half quarter emark2 muchless muchgreater '
++ 'shaded shaded2 shaded3 p1 p2 p3 p4 p5 p6 p7 p8 p9 p10 p11 p12 p13 '
++ 'p14 p15 p16 p17 p18 p19 p20 p21 p22 p23 p24 p25 p26 p27 p28 p29 '
++ 'p30 p31 p32 p33 p34 p35 p36 p37 p38 p39 p40 p41 p42 p43 p44 p45 '
++ 'alpha beta Gamma Pi Sigma sigma mu tau Phi theta Omega delta inf ninf in intersect '
++ 'equiv pm gteq lteq upperint lowerint div approx degree cdot hyphen sqrt endquote power2 block space3';
+
+
+export class Font {
+  constructor(fontImage, canvasContext) {
+    this.fontImage = fontImage;
+    this.ctx = canvasContext;
+  }
+
+  generateCharacterCache() {
+    this.characterCache = new Map();
+
+    for (let [i, char] of CHARS.split(' ').entries()) {
+      let x = i % NUM_PER_ROW;
+      let y = Math.floor(i / NUM_PER_ROW);
+      this.characterCache.set(char, {x: x * CHAR_WIDTH, y: y * CHAR_HEIGHT});
+    }
+  }
+
+  drawChar(x, y, char) {
+    if (this.characterCache == null)
+      this.generateCharacterCache();
+
+    let rect = {...this.characterCache.get(char), w: CHAR_WIDTH, h: CHAR_HEIGHT};
+
+    this.ctx.drawImage(this.fontImage, rect.x, rect.y, rect.w, rect.h, x, y, rect.w, rect.h);
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,55 @@
+
+import {Font} from './font.js';
+
+function loadImage(path) {
+  return new Promise((resolve, reject) => {
+    let image = new Image();
+    image.src = path;
+    image.onload = () => resolve(image);
+    image.onerror = () => reject();
+  });
+}
+
+const maskOutColor = (imageData, color) => {
+  const getColorIndicesForCoord = (x, y, width) => {
+    const red = y * (width * 4) + x * 4;
+    return [red, red + 1, red + 2, red + 3];
+  };
+
+  for (let x = 0; x < imageData.width; ++x) {
+    for (let y = 0; y < imageData.height; ++y) {
+      const [redIndex, greenIndex, blueIndex, alphaIndex] = getColorIndicesForCoord(x, y, imageData.width);
+      const [red, green, blue] = [imageData.data[redIndex], imageData.data[greenIndex], imageData.data[blueIndex]];
+      if (red === color.r && green === color.g && blue === color.b) {
+        imageData.data[alphaIndex] = 0;
+      }
+    }
+  }
+};
+
+const getMaskedCanvas = (image, color) => {
+  let canvas = document.createElement('canvas');
+  let ctx = canvas.getContext('2d');
+  canvas.width = image.width;
+  canvas.height = image.height;
+  ctx.drawImage(image, 0, 0);
+  let imageData = ctx.getImageData(0, 0, image.width, image.height);
+  maskOutColor(imageData, color);
+  ctx.putImageData(imageData, 0, 0);
+  return canvas;
+};
+
+window.onload = () => {
+  let canvas = document.querySelector('#game');
+  let ctx = canvas.getContext('2d');
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  loadImage('images/curses_800x600.bmp')
+    .then(image => {
+      let maskedCanvas = getMaskedCanvas(image, {r: 255, g: 0, b: 255});
+
+      let font = new Font(maskedCanvas, ctx);
+      font.drawChar(200, 200, 'dwarf');
+    });
+};
