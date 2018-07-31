960e4fda5f70a5b4092b4fc2cdb06b49c14600cd
Joe Bentley <jbentley@star.sr.bham.ac.uk>
2018-07-31 13:50:22 +0100

    Added gruntfile for tests

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+const webpackOfflineTestConfig = require('./webpack.offline.test.config');
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    simplemocha: {
+      all: {
+        src: ['tests/dist/bundle.js']
+      }
+    },
+    webpack: {
+      dev: webpackOfflineTestConfig
+    },
+    watch: {
+      scripts: {
+        files: ['**/*.js'],
+        tasks: ['test'],
+        // options: {
+        //   spawn: false,
+        // },
+      },
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('grunt-webpack');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('test', ['webpack', 'simplemocha']);
+};
